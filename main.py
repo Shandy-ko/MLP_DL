23a308f325b9c4069d46753fa36b1468a16dfc48
Shandy-ko <shandy1124gaff@gmail.com>
2016-02-03 23:08:19 +0900

    fourth

--- main.py
+++ main.py
@@ -41,3 +41,30 @@ def grad(x,d,w,b):
 
 x,d,w,b=md.main()
 grad(x,d,w,b)
+
+
+
+def SGD(x, d, w, b, e, eta=0.10, iteration=5, minibatch_size=10):
+    '''
+    Function: SGD
+    Summary: 確率的勾配法 + ミニバッチ
+    Attributes:
+        @param (x):データ
+        @param (d):ラベル
+        @param (w):重み
+        @param (b):バイアス
+        @param (e):誤差を保存
+        @param (eta) default=0.10: 学習係数
+        @param (iteration) default=5: イテレーション
+        @param (minibatch_size) default=10: ミニバッチのサイズ
+    Returns: 誤差, 重み, バイアス
+    '''
+    for _ in range(iteration):
+        for index in range(0, x.shape[0], minibatch_size):
+            _x = x[index:index + minibatch_size]
+            _d = d[index:index + minibatch_size]
+            w_grad, b_grad = grad(_x, _d, w, b)
+            w -= eta * w_grad
+            b -= eta * b_grad
+            e.append(np.mean(np.abs(d - p_y_given_x(x, w, b))))
+    return e, w, b
