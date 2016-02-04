b11ac4bb234bc97591567d198b2e32b0959081f3
Shandy-ko <shandy1124gaff@gmail.com>
2016-02-04 22:34:33 +0900

    sixth

--- function.py
+++ function.py
@@ -42,7 +42,29 @@ def grad(x,d,w,b):
   return w_grad, b_grad
 
 
-def SGD(x, d, w, b, e, eta=0.10, iteration=5, minibatch_size=10):
+def GD(x,d,w,b,e, eta=0.10, iteration=700):
+  '''
+  Function: GD
+  Summary: 勾配法
+  Attributes:
+      @param (x):データ
+      @param (d):ラベル
+      @param (w):重み
+      @param (b):バイアス
+      @param (e):誤差を保存
+      @param (eta) default=0.10: 学習係数
+      @param (iteration) default=700: イテレーション
+  Returns: 誤差，重み，バイアス
+  '''
+  for _ in range(iteration):
+    w_grad, b_grad = grad(x,d,w,b)
+    w -= eta*w_grad
+    b -= eta*b_grad
+    e.append(np.mean(np.abs(d-p_y_given_x(x,w,b))))
+  return e,w,b
+
+
+def SGD(x,d,w,b,e, eta=0.10, iteration=5, minibatch_size=10):
     '''
     Function: SGD
     Summary: 確率的勾配法 + ミニバッチ
@@ -55,15 +77,15 @@ def SGD(x, d, w, b, e, eta=0.10, iteration=5, minibatch_size=10):
         @param (eta) default=0.10: 学習係数
         @param (iteration) default=5: イテレーション
         @param (minibatch_size) default=10: ミニバッチのサイズ
-    Returns: 誤差, 重み, バイアス
+    Returns: 誤差，重み，バイアス
     '''
     for _ in range(iteration):
         for index in range(0, x.shape[0], minibatch_size):
-            _x = x[index:index + minibatch_size]
-            _d = d[index:index + minibatch_size]
-            w_grad, b_grad = grad(_x, _d, w, b)
-            w -= eta * w_grad
-            b -= eta * b_grad
-            e.append(np.mean(np.abs(d - p_y_given_x(x, w, b))))
+            _x = x[index:index+minibatch_size]
+            _d = d[index:index+minibatch_size]
+            w_grad, b_grad = grad(_x,_d,w,b)
+            w -= eta*w_grad
+            b -= eta*b_grad
+            e.append(np.mean(np.abs(d-p_y_given_x(x, w, b))))
             #absolute Calculate the absolute value element-wise.
     return e, w, b
--- main.py
+++ main.py
@@ -9,8 +9,12 @@ def main():
   Summary: メイン関数
   '''
 
-  m, N=2, 10
+  m, N=2, 10000
   x1, x2=np.random.randn(N,m), np.random.randn(N,m)+np.array([5,5])
+  # print("x1=%s"%x1)
+  # print("--------------")
+  # print("x2=%s"%x2)
+  # print("--------------")
   #randn 標準正規分布による (N * m) の行列
   x=np.vstack((x1,x2))
   #vstack Stack arrays in sequence vertically (row wise).
@@ -20,24 +24,25 @@ def main():
   d=np.hstack((d1,d2))
   #hstack Stack arrays in sequence horizontally (column wise).
   dataset=np.column_stack((x,d))
+  print("dataset=\n%s"%dataset)
   #colomn_stack Stack arrays in sequence horizontally (column wise), just like with hstack.
   np.random.shuffle(dataset)
-  print("dataset=\n%s"%dataset)
+  # print("dataset=\n%s"%dataset)
 
   x, d=dataset[:,:2], dataset[:,2]
   w, b=np.random.rand(m), np.random.random()
-  print("x=%s"%x)
-  print("--------------")
-  print("d1=%s"%d1)
-  print("--------------")
-  print("d2=%s"%d2)
-  print("--------------")
-  print("d=%s"%d)
-  print("--------------")
-  print("w=%s"%w)
-  print("--------------")
-  print("b=%s"%b)
-  # e, w, b = func.GD(x, d, w, b, list())
+  # print("x=%s"%x)
+  # print("--------------")
+  # print("d1=%s"%d1)
+  # print("--------------")
+  # print("d2=%s"%d2)
+  # print("--------------")
+  # print("d=%s"%d)
+  # print("--------------")
+  # print("w=%s"%w)
+  # print("--------------")
+  # print("b=%s"%b)
+  e, w, b = func.GD(x, d, w, b, list())
   e, w, b = func.SGD(x, d, w, b, list())
   # e, w, b = func.SGD_momentum(x, d, w, b, list())
   # e, w, b = func.SGD_adagrad(x, d, w, b, list())
@@ -58,16 +63,16 @@ def plot(x,d,x1,x2,e,w,b):
   '''
   print np.mean(np.abs(d-func.p_y_given_x(x,w,b)))
     #mean average
-  # plt.plot(e)
-  # plt.show()
-  # bx=np.arange(-6,10,0.1)
-  # by= -b/w[1] - w[0]/[1]*bx
-  # plt.xlim([-5,10])
-  # plt.ylim([-5,9])
-  # plt.plot(bx,by)
-  # plt.scatter(x1[:,0],x1[:,1],c='g')
-  # plt.scatter(x1[:,0],x1[:,1],c='r')
-  # plt.show
+  plt.plot(e)
+  plt.show()
+  bx=np.arange(-6,10,0.1)
+  by= -b/w[1] - w[0]/[1]*bx
+  plt.xlim([-5,10])
+  plt.ylim([-5,9])
+  plt.plot(bx,by)
+  plt.scatter(x1[:,0],x1[:,1],c='g')
+  plt.scatter(x1[:,0],x1[:,1],c='r')
+  plt.show
 
 if __name__=="__main__":
   main()
